c61ee4f3733499cd6e71a2cf93e1ff6e40b8a10a
Mike Peiman <mikepeiman@gmail.com>
2023-10-05 09:33:25 -0400

    fixed typo in svetlte.config

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import adapter from "@sveltejs/adapter-netlify";
 export default {
   kit: {
     adapter: adapter(),
-  }
+  },
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
