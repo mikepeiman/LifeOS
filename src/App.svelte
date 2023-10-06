d9e114ce32904ef466784750bde83103d85d3b9c
Mike Peiman <mikepeiman@gmail.com>
2023-10-06 11:35:07 -0400

    fixed log

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,11 @@
   let isProd = location.hostname !== "localhost";
   console.log(`🚀 ~ file: App.svelte:4 ~ location.hostname:`, location.hostname)
   let cssPath = isProd ? "/output.css" : "/dist/output.css";
+  console.log(`🚀 ~ file: App.svelte:6 ~ cssPath:`, cssPath)
 </script>
 
 <svelte:head>
   <link href="{cssPath}" rel="stylesheet" />
-  console.log(`🚀 ~ file: App.svelte:9 ~ cssPath:`, cssPath)
 </svelte:head>
 
 <main class="w-screen p-0 h-full  bg-white bg-gradient-to-br from-blue-200 to-purple-200">
